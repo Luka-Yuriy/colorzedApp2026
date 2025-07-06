3b080dceb54a6344394fd13504de6e4d2d565a08
Yuri Luka <lukayurii.work@gmail.com>
2025-07-06 13:12:58 +0500

    Added connection with code

--- colorzedApp2026/ViewController.swift
+++ colorzedApp2026/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorizedView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenLabelValue: UILabel!
+    @IBOutlet var blueLabelValue: UILabel!
+    
+    @IBOutlet var redSluder: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func redSliderAction() {
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    
+    @IBAction func blueSliderAction() {
+    }
 }
 
