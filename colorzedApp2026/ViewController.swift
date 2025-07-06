395dedc56aaea74d14dda0ae64782a56a1eaaf68
Yuri Luka <lukayurii.work@gmail.com>
2025-07-06 14:25:43 +0500

    Work with logic

--- colorzedApp2026/ViewController.swift
+++ colorzedApp2026/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: IBOutlets
     @IBOutlet var colorizedView: UIView!
     
     @IBOutlet var redLabel: UILabel!
@@ -15,24 +17,49 @@ class ViewController: UIViewController {
     @IBOutlet var blueLabel: UILabel!
     
     @IBOutlet var redValueLabel: UILabel!
-    @IBOutlet var greenLabelValue: UILabel!
-    @IBOutlet var blueLabelValue: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
     
-    @IBOutlet var redSluder: UISlider!
+    @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    //MARK: Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setBackgroundColor()
+        setValueForRGBLabel()
     }
+    
+    //MARK: IBActions
     @IBAction func redSliderAction() {
+        setBackgroundColor()
     }
     
     @IBAction func greenSliderAction() {
+        setBackgroundColor()
     }
     
     @IBAction func blueSliderAction() {
+        setBackgroundColor()
+    }
+    
+    @IBAction func setValueForRGBLabel() {
+        redValueLabel.text = (String(format: "%.2f", redSlider.value))
+        greenValueLabel.text = (String(format: "%.2f", greenSlider.value))
+        blueValueLabel.text = (String(format: "%.2f", blueSlider.value))
+    }
+    
+    //MARK: Private Methods
+    private func setBackgroundColor() {
+        colorizedView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
 }
+
+
 
